48f612a6179b0b12f1a18830d18088ac59bcd763
Chip Lynch <chip@chiplynch.com>
2020-12-05 21:23:32 -0800

    day6 part2

--- day6/runme.py
+++ day6/runme.py
@@ -39,16 +39,46 @@ class part1:
         print(f'count: {self.result}')
         return(self.result)
 
+class part2:
+    """class to solve the problem at hand
+    """
+
+    def __init__(self, filename):
+        self.result = 0  # Highest Seat ID
+        self.mydata = filethingy(filename)
+        self.groups = []
 
 
-test = filethingy('testinput.txt')
+    def solve(self):
+        allanswers = set('')
+        self.mydata.values.append('')
+        newpass = True
+        for boardingPass in self.mydata.values:
+            if newpass:
+                allanswers = set(boardingPass)
+                # print(f"allanswers starts as {allanswers}")
+            if boardingPass == '':
+                thiscount = len(set(allanswers))
+                self.result = self.result + thiscount
+                print(f"Adding {thiscount} for {allanswers}")
+                allanswers = set('')
+                newpass = True
+            else:
+                newpass = False
+                allanswers = allanswers.intersection(boardingPass.strip())
+                print(f"handling: {boardingPass}: {allanswers}")
 
-mything = part1('testinput.txt')
-test1 = mything.solve()
-mything = part1('input.txt')
-part1 = mything.solve()
+        print(f'count: {self.result}')
+        return(self.result)
 
-# mything = part2('testinput.txt')
-# test2 = mything.solve()
-# mything = part2('input.txt')
-# part2 = mything.solve()
+# test = filethingy('testinput.txt')
+
+# mything = part1('testinput.txt')
+# test1 = mything.solve()
+# mything = part1('input.txt')
+# part1 = mything.solve()
+
+mything = part2('testinput.txt')
+test2 = mything.solve()
+mything = part2('input.txt')
+part2 = mything.solve()
