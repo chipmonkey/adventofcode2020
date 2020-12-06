da4c5f221726aa7a5b48dc977858b0e919f8570d
Chip Lynch <chip@chiplynch.com>
2020-12-05 21:09:31 -0800

    day6 part1

---
+++ day6/runme.py
@@ -0,0 +1,54 @@
+import re
+
+DEBUG = False
+
+class filethingy:
+
+    def __init__(self, filename):
+        self.values = []
+        f = open(filename, 'r')
+        for line in f:
+            self.values.append(line.strip())
+        print("input: ", self.values)
+
+
+
+class part1:
+    """class to solve the problem at hand
+    """
+
+    def __init__(self, filename):
+        self.result = 0  # Highest Seat ID
+        self.mydata = filethingy(filename)
+        self.groups = []
+
+
+    def solve(self):
+        thisgroup = ''
+        self.mydata.values.append('')
+        for boardingPass in self.mydata.values:
+            print(boardingPass)
+            if boardingPass == '':
+                thiscount = len(set(thisgroup))
+                self.result = self.result + thiscount
+                print(f"Adding {thiscount} for {thisgroup}")
+                thisgroup = ''
+            else:
+                thisgroup = thisgroup + boardingPass.strip()
+
+        print(f'count: {self.result}')
+        return(self.result)
+
+
+
+test = filethingy('testinput.txt')
+
+mything = part1('testinput.txt')
+test1 = mything.solve()
+mything = part1('input.txt')
+part1 = mything.solve()
+
+# mything = part2('testinput.txt')
+# test2 = mything.solve()
+# mything = part2('input.txt')
+# part2 = mything.solve()
